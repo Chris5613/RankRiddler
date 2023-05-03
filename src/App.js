72188b13721a115035aa5a7f0526d801fa1652a9
Christian Wu <christianwu56@gmail.com>
2023-05-03 11:34:12 -0700

    starting on the home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./Components/Nav";
 import Valorant from "./Components/Valorant";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Nav />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/valorant" element={<Valorant />} />
         </Routes>
     </BrowserRouter>
---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <img src="https://o.remove.bg/downloads/682d7b21-53f9-478e-bbed-57f01a5186ee/5849a85085ebe274f33807c596c43309-removebg-preview.png" alt="" />
+    </div>
+  )
+}
+
+export default Home
