0c57d3a42c506a47b70cfeba44de1f0ae4718844
Christian Wu <christianwu56@gmail.com>
2023-05-05 10:39:50 -0700

    first draft of the leaderboard

--- src/Components/Leaderboard.jsx
+++ src/Components/Leaderboard.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
 const Leaderboard = () => {
+  const mockData = [
+    { username: 'John', score: 200 },
+    { username: 'Jane', score: 150 },
+    { username: 'Bob', score: 100 },
+    { username: 'Alice', score: 50 },
+  ];
+
   return (
-    <div>
-      <h1>Coming Soon!</h1>
+    <div className="leaderboard-container">
+      <table className='alltime'>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Username</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mockData.map((data, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{data.username}</td>
+              <td>{data.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
