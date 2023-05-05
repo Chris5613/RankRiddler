6c2917ebac47d4bbe8d305b6b3bbc4a81550d017
Christian Wu <christianwu56@gmail.com>
2023-05-04 22:18:03 -0700

    both csgo and league forms

--- src/Components/Submit.jsx
+++ src/Components/Submit.jsx
@@ -10,7 +10,6 @@ function Submit() {
   return (
     <>
     <div className='select-game'>
-      <h2 style={{color: "#fff"}}>Select a game</h2>
       <select className ="select" value={game} onChange={handleGameChange}>
         <option value="">-- Select a game --</option>
         <option value="lol">League of Legends</option>
@@ -35,10 +34,106 @@ function Submit() {
 }
 
 function LeagueOfLegendsForm() {
+  const [youtubeLink, setYoutubeLink] = useState('');
+  const [userName, setUserName] = useState('');
+  const [playerInfo, setPlayerInfo] = useState('');
+  const [selectedRank, setSelectedRank] = useState('');
+  const [discordLink, setDiscordLink] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleYoutubeLinkChange = (event) => {
+    setYoutubeLink(event.target.value);
+  };
+
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const handlePlayerInfoChange = (event) => {
+    setPlayerInfo(event.target.value);
+  };
+
+  const handleRankChange = (event) => {
+    setSelectedRank(event.target.value);
+  };
+
+  const handleDiscordLinkChange = (event) => {
+    setDiscordLink(event.target.value);
+  };
+
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can handle the form submission logic
+  };
   return (
-    <div>
-      <h2>League of Legends Form</h2>
-      {/* Render the form fields for League of Legends here */}
+    <div className='form-container'>
+      <form onSubmit={handleSubmit} className='form'>
+        <label className='form-label'>
+          YouTube link: <span style={{color: "#e34234"}}>*</span>
+          </label>
+          <input
+            className="form-input"
+            type="text"
+            value={youtubeLink}
+            onChange={handleYoutubeLinkChange}
+          />
+        <br />
+
+        <label className='form-label'>
+          User's Name: <span style={{color: "#e34234"}}>*</span>
+        </label>
+          <input className="form-input" type="text" value={userName} onChange={handleUserNameChange} />
+        <br />
+
+        <label className='form-label'>
+          Player info: <span style={{color: "#e34234"}}>*</span>
+        </label>
+          <textarea value={playerInfo} onChange={handlePlayerInfoChange} />
+        <br />
+
+        <label className='form-label'> Select a rank: <span style={{color: "#e34234"}}>*</span></label>
+          <select value={selectedRank} onChange={handleRankChange}>
+            <option value="">Select a rank</option>
+            <option value="Iron">Iron</option>
+            <option value="Bronze">Bronze</option>
+            <option value="Silver">Silver</option>
+            <option value="Gold">Gold</option>
+            <option value="Platinum">Platinum</option>
+            <option value="Diamond">Diamond</option>
+            <option value="Immortal">Master</option>
+            <option value="Radiant">Grandmaster</option>
+            <option value="Challenger">Challenger</option>
+          </select>
+        <br />
+
+        <label className='form-label'>
+          Tracker.gg Link: <span style={{color: "#e34234"}}>*</span>
+        </label>
+          <input
+            className="form-input"
+            type="text"
+            value={discordLink}
+            onChange={handleDiscordLinkChange}
+          />
+        <br />
+
+          <input
+            type="checkbox"
+            className='form-checkbox'
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+          />
+          <label className='form-label'>I agree to the terms and conditions</label>
+        <br />
+
+        <button type="submit" disabled={!isChecked}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
@@ -148,10 +243,106 @@ function ValorantForm() {
 }
 
 function CSGOForm() {
+  const [youtubeLink, setYoutubeLink] = useState('');
+  const [userName, setUserName] = useState('');
+  const [playerInfo, setPlayerInfo] = useState('');
+  const [selectedRank, setSelectedRank] = useState('');
+  const [discordLink, setDiscordLink] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleYoutubeLinkChange = (event) => {
+    setYoutubeLink(event.target.value);
+  };
+
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const handlePlayerInfoChange = (event) => {
+    setPlayerInfo(event.target.value);
+  };
+
+  const handleRankChange = (event) => {
+    setSelectedRank(event.target.value);
+  };
+
+  const handleDiscordLinkChange = (event) => {
+    setDiscordLink(event.target.value);
+  };
+
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can handle the form submission logic
+  };
   return (
-    <div>
-      <h2>CS:GO Form</h2>
-      {/* Render the form fields for CS:GO here */}
+    <div className='form-container'>
+      <form onSubmit={handleSubmit} className='form'>
+        <label className='form-label'>
+          YouTube link: <span style={{color: "#e34234"}}>*</span>
+          </label>
+          <input
+            className="form-input"
+            type="text"
+            value={youtubeLink}
+            onChange={handleYoutubeLinkChange}
+          />
+        <br />
+
+        <label className='form-label'>
+          User's Name: <span style={{color: "#e34234"}}>*</span>
+        </label>
+          <input className="form-input" type="text" value={userName} onChange={handleUserNameChange} />
+        <br />
+
+        <label className='form-label'>
+          Player info: <span style={{color: "#e34234"}}>*</span>
+        </label>
+          <textarea value={playerInfo} onChange={handlePlayerInfoChange} />
+        <br />
+
+        <label className='form-label'> Select a rank: <span style={{color: "#e34234"}}>*</span></label>
+          <select value={selectedRank} onChange={handleRankChange}>
+            <option value="">Select a rank</option>
+            <option value="Silver">Silver</option>
+            <option value="Silver Elite">Silver Elite</option>
+            <option value="Gold Nova">Gold Nova</option>
+            <option value="aster Guradian">Master Guradian</option>
+            <option value="Master Guardian Elite">Master Guardian Elite</option>
+            <option value="DMG">DMG</option>
+            <option value="Legendary Eagle">Legendary Eagle</option>
+            <option value="Supreme">Supreme</option>
+            <option value="Global Elite">Global Elite</option>
+          </select>
+        <br />
+
+        <label className='form-label'>
+          Tracker.gg Link: <span style={{color: "#e34234"}}>*</span>
+        </label>
+          <input
+            className="form-input"
+            type="text"
+            value={discordLink}
+            onChange={handleDiscordLinkChange}
+          />
+        <br />
+
+          <input
+            type="checkbox"
+            className='form-checkbox'
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+          />
+          <label className='form-label'>I agree to the terms and conditions</label>
+        <br />
+
+        <button type="submit" disabled={!isChecked}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
