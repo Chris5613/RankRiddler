137094ab61fdc59e56593e807fefd185f96e54c2
Christian Wu <christianwu56@gmail.com>
2023-05-08 19:06:32 -0700

    fixed rerendering issue

--- Back-End/server.js
+++ Back-End/server.js
@@ -31,5 +31,11 @@ mongoose.connection.on("error", (err) => {
   process.exit();
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use("/", userRoutes);
 app.use("/form", formRoutes);
