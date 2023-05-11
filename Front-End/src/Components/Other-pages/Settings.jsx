5439bde0de37dd233bb6d15a22c02535f893a19b
Christian Wu <christianwu56@gmail.com>
2023-05-10 19:49:37 -0700

    change api calls

--- Front-End/src/Components/Other-pages/Settings.jsx
+++ Front-End/src/Components/Other-pages/Settings.jsx
@@ -48,7 +48,7 @@ const Settings = () => {
 
   const saveUser = async (username, score) => {
     try {
-      const response = await fetch('http://localhost:3001/saveuser', {
+      const response = await fetch('https://rr-back-end.onrender.com/saveuser', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
