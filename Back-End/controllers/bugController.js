74b3095d5a2547162ad32c1d38eb9ff3f48f3aa4
Milan Lakhani <milanlakhaniaiml@gmail.com>
2023-05-14 22:07:25 -0400

    Created report endpoint on backend

--- Back-End/controllers/bugController.js
+++ Back-End/controllers/bugController.js
@@ -1,8 +1,19 @@
 const bug = require("../models/bugForm");
+const report = require("../models/reportForm");
 
 const bugForm = async (req, res) => {
   try {
     const form = new bug(req.body);
+    await form.save();
+    res.status(201).json({ form });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const reportForm = async (req, res) => {
+  try {
+    const form = new report(req.body);
     await form.save();
     res.status(201).json({ form });
   } catch (error) {
---
+++ Back-End/models/reportForm.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const reportSchema = new Schema({
+    youtubeLink: { type: String, required: true },
+    playerInfo: { type: String, required: true },
+    reportedBy: { type: String, required: true },
+    reportedAt: { type: Date, default: Date.now() },
+});
+
+const reportForm = mongoose.model("report", reportSchema);
+
+module.exports = reportForm;
