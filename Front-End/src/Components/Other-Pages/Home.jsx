9b36510b27b98a6e70eaa3682c18a9798f16393d
Christian Wu <christianwu56@gmail.com>
2023-05-16 11:12:24 -0700

    sitemap and made the cards on the homepage bigger

--- Front-End/src/Components/Other-Pages/Home.jsx
+++ Front-End/src/Components/Other-Pages/Home.jsx
@@ -16,26 +16,26 @@ const Home = () => {
             <div className='image-row '>
               <NavLink to="/valorant">
                 <img
-                  className="main-images"
+                  className="main-images width"
                   src="https://cdn1.epicgames.com/offer/cbd5b3d310a54b12bf3fe8c41994174f/valtall_1200x1600-300d8e4cb9bee9dbb1166b574b3bdc93"
                   alt="valorant"
                 />
               </NavLink>
               <NavLink to="/league">
                 <img
-                  className="main-images"
+                  className="main-images width"
                   src="https://cdn1.epicgames.com/offer/24b9b5e323bc40eea252a10cdd3b2f10/LoL_1200x1600-15ad6c981af8d98f50e833eac7843986"
                   alt="league"
                 />
               </NavLink>
               <NavLink to="/csgo">
                 <img
-                  className="main-images"
-                  src="https://downloadwap.com/thumbs4/games/preview/2020j/img/133656_counter-st_1.jpg"
+                  className="main-images width"
+                  src="https://i.gyazo.com/49247bef6608e9c0004372c93bd966c1.png"
                   alt="csgo"
                 />
               </NavLink>
-              <div className='coming-soon'>
+              {/* <div className='coming-soon'>
                 <span className="image-title">Coming Soon</span>
               </div>
               <div className='coming-soon'>
@@ -43,7 +43,7 @@ const Home = () => {
               </div>
               <div className='coming-soon'>
                 <span className="image-title">Coming Soon</span>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
