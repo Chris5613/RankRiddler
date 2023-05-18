0bd8ef4fd8fc4728189e1163a88d5b31ea960bf2
Milan Lakhani <milanlakhaniaiml@gmail.com>
2023-05-18 14:43:33 -0400

    added youtube link validations

--- Back-End/controllers/formControllers.js
+++ Back-End/controllers/formControllers.js
@@ -2,20 +2,104 @@ const val = require("../models/valForm");
 const league = require("../models/leagueForm");
 const csgo = require("../models/csgoForm");
 
+/**
+ * 
+ * @param {String} game 
+ * @param {String} youtubeCode 
+ * @returns false if youtubeCode is not found in the database for the game 
+ */
+const findYoutubeCode = async (game, youtubeCode) => {
+  try {
+    switch (game) {
+      case "csgo":
+        const resCS = await csgo.findOne({ "youtubeLink": { $regex: youtubeCode, $options: 'i' } });
+        console.log("csgo findYoutubeLink", youtubeCode, game);
+        console.log(resCS);
+        if (resCS) {
+          console.log('csgo faliled - return true')
+          return true;
+        }
+        return false;
+      case "val":
+        const resVal = await val.findOne({ "youtubeLink": { $regex: youtubeCode, $options: 'i' } });
+        if (resVal) {
+          return true;
+        }
+        return false;
+      case "league":
+        const resLeague = await league.findOne({ "youtubeLink": { $regex: youtubeCode, $options: 'i' } });
+        if (resLeague) {
+          return true;
+        }
+        return false;
+      default:
+        return true;
+    }
+  } catch (error) {
+    return true;
+  }
+}
+
+/**
+ * 
+ * @param {{youtubeLink, playerInfo, rank}} game 
+ * @returns true if passed all validations
+ */
+const validations = async (game) => {
+  let ytRegex = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/
+  ytRegex = new RegExp(ytRegex);
+  let matches = game.youtubeLink.match(ytRegex);
+  if (matches) {
+    try {
+      if (await findYoutubeCode('csgo', matches[1])) {
+        console.log("csgo faliled")
+        return false;
+      }
+      if (await findYoutubeCode('val', matches[1])) {
+        console.log("val faliled")
+        return false;
+      }
+      if (await findYoutubeCode('league', matches[1])) {
+        console.log("league faliled")
+        return false;
+      }
+
+      // Access the game object and add new validations below
+
+      return true;
+    }
+    catch (err) {
+      return false;
+    }
+  }
+  return false;
+}
+
 const valForm = async (req, res) => {
   try {
-    const form = new val(req.body);
-    await form.save();
-    res.status(201).json({ form });
+    if (await validations(req.body)) {
+      const form = new val(req.body);
+      await form.save();
+      res.status(201).json({ form });
+    } else {
+      res.status(400).json({ error: "Invalid submition" });
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
+
 const leagueForm = async (req, res) => {
   try {
-    const form = new league(req.body);
-    await form.save();
-    res.status(201).json({ form });
+    console.log("league form");
+    console.log(req.body);
+    if (await validations(req.body)) {
+      const form = new league(req.body);
+      await form.save();
+      res.status(201).json({ form });
+    } else {
+      res.status(400).json({ error: "Invalid submition" });
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -23,9 +107,13 @@ const leagueForm = async (req, res) => {
 
 const csgoForm = async (req, res) => {
   try {
-    const form = new csgo(req.body);
-    await form.save();
-    res.status(201).json({ form });
+    if(await validations(req.body)){
+      const form = new csgo(req.body);
+      await form.save();
+      res.status(201).json({ form });
+    } else {
+      res.status(400).json({ error: "Invalid submition" });
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
