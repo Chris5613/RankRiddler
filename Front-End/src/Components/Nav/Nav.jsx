3fefcdd256e81d32d74d05a5ecbe381fc143b8ae
Christian Wu <christianwu56@gmail.com>
2023-05-18 09:27:55 -0700

    changing navbar around

--- Front-End/src/Components/Nav/Nav.jsx
+++ Front-End/src/Components/Nav/Nav.jsx
@@ -6,7 +6,6 @@ const Nav = () => {
   return (
     <>
       <header>
-        <div className="header-container">
           <div className="nav-container">
             <nav className="nav">
               <ul>
@@ -45,7 +44,7 @@ const Nav = () => {
                         strokeLinejoin="round"
                       ></path>
                     </svg>
-                    Choose a game
+                    Games
                   </NavLink>
                 </li>
                 <li>
@@ -87,13 +86,9 @@ const Nav = () => {
                     Multiplayer
                   </NavLink>
                 </li>
-              </ul>
-            </nav>
-            <a href="/">
-              <img src={logo} alt="logo" className="logo" />
-            </a>
-            <div className="actions">
-              <ul>
+                <a href="/">
+                  <img src={logo} alt="logo" className="logo" />
+                </a>
                 <li>
                   <NavLink to="/submit" className="nav-link">
                     <svg
@@ -141,7 +136,7 @@ const Nav = () => {
                 <li>
                   <NavLink to="/howto" className="nav-link">
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       width="17"
                       height="17"
                       viewBox="0 0 17 17"
@@ -151,27 +146,26 @@ const Nav = () => {
                       <path
                         d="M8.5 15.7379C12.4975 15.7379 15.7381 12.4973 15.7381 8.49981C15.7381 4.50232 12.4975 1.26172 8.5 1.26172C4.50251 1.26172 1.2619 4.50232 1.2619 8.49981C1.2619 12.4973 4.50251 15.7379 8.5 15.7379Z"
                         stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       ></path>
                       <path
                         d="M8.49998 9.40444V8.49968L9.77932 7.22034C10.1187 6.88107 10.3094 6.42089 10.3095 5.94101V5.78539C10.3095 5.23077 9.99646 4.72411 9.50065 4.4762L9.30884 4.3803C9.05768 4.25479 8.78076 4.18945 8.49998 4.18945C8.21921 4.18945 7.94229 4.25479 7.69113 4.3803L7.59522 4.42825C7.0406 4.70511 6.69046 5.27239 6.69046 5.89215V6.69015"
                         stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       ></path>
                       <path
                         d="M8.50001 13.0234C8.99969 13.0234 9.40477 12.6183 9.40477 12.1186C9.40477 11.6189 8.99969 11.2139 8.50001 11.2139C8.00032 11.2139 7.59525 11.6189 7.59525 12.1186C7.59525 12.6183 8.00032 13.0234 8.50001 13.0234Z"
                         fill="currentColor"
                       ></path>
                     </svg>
-                    How to play
+                    Help
                   </NavLink>
                 </li>
               </ul>
-            </div>
+            </nav>
           </div>
-        </div>
       </header>
     </>
   );
