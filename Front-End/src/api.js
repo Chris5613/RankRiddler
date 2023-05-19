26e00e9cf5ce0bf785c469beef95d61debef4bb8
Milan Lakhani <milanlakhaniaiml@gmail.com>
2023-05-19 15:03:26 -0400

    Added Endpoints

---
+++ Front-End/src/api.js
@@ -0,0 +1,23 @@
+//Production
+const root = 'https://rr-back-end.onrender.com';
+
+//Development
+// const root = 'http://localhost:3001';
+
+const API = {
+    ValorantSubmit: `${root}/form/val`,
+    LeagueSubmit: `${root}/form/league`,
+    CsgoSubmit: `${root}/form/csgo`,
+    GetValorantData: `${root}/form/valdata`,
+    GetLeagueData: `${root}/form/leaguedata`,
+    GetCsgoData: `${root}/form/csgodata`,
+    BugSubmit: `${root}/form/bug`,
+    ReportSubmit: `${root}/form/report`,
+    GetAllUsers: `${root}/allusers`,
+    GetUserByUsername: `${root}/user`,
+    SaveUser: `${root}/saveuser`,
+    GetUserByUuid: `${root}/user`,
+    UpdatePoints: `${root}/updatepoints`,
+}
+
+export default API;
