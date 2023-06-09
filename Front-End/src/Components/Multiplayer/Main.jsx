8fa16e54b2cbe496abed692890a520fc32498070
Shubham Chauhan <shubham.chauhan826@gmail.com>
2023-06-09 20:25:50 +0530

    Added Multiplayer UI

--- Front-End/src/Components/Multiplayer/Main.jsx
+++ Front-End/src/Components/Multiplayer/Main.jsx
@@ -3,9 +3,19 @@ import React from 'react';
 const Main = () => {
   return (
     <div id="main-h1">
-      <h1>Coming Soon...</h1>
-      <br />
-      <h1>Multiplayer 1v1s</h1>
+      <h6>1v1 Multiplayer</h6>
+
+      <div className='multiplayer-btn-container'>
+        <button className="multiplayer-btn">
+          Find A Player
+        </button>
+        <button className="multiplayer-btn">
+          Room List
+        </button>
+        <button className="multiplayer-btn">
+          Battle Friend
+        </button>
+      </div>
     </div>
   );
 };
