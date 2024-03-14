0f52abcd21ffa23b9bc2af2767a9fba1a4fd7efc
Christian Wu <christian.wu@galvanize.com>
2024-03-14 15:56:42 -0700

    two players can now connect

--- Front-End/src/Components/Multiplayer/Gamepage.jsx
+++ Front-End/src/Components/Multiplayer/Gamepage.jsx
@@ -1,12 +1,30 @@
 import { useSocket } from '../SocketContext';
 import {useEffect,useState} from 'react';
 import Loader from '../Loader/Loader';
-
+import {useSelector } from 'react-redux';
+import API from '../../api';
 
 const Gamepage = () => {
+  
   const [opponent, setOpponent] = useState('');
   const [loading, setLoading] = useState(true);
   const socket = useSocket();
+  const userId = useSelector((state) => state.settings.userId);
+  const [username, setUsername] = useState('')
+
+  useEffect(() => {
+    const getOneUser = async (uuid) => {
+      const response = await fetch(`${API.GetUserByUuid}/${uuid}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      setUsername(data.username)
+    };
+    getOneUser(userId);
+  }, [userId])
 
     useEffect(() => {
     socket.on('matchFound', (data) => {
@@ -21,13 +39,17 @@ const Gamepage = () => {
   console.log(opponent)
   return (
     <div>
-      {loading ? (
-            <Loader />
-          ) : (
-            <>
-              {opponent}
-            </>
-          )}
+      <div >
+        {loading ? (
+        <div className='loading'>
+          <Loader />      
+        </div>
+        ) : (
+          <>
+            {username} vs {opponent}
+          </>
+        )}
+      </div>  
     </div>
   )
 }
