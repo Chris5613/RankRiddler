5e48b69b7b3ad6ee455e9abdb69c6a5777c6f8f1
Christian Wu <christian.wu@galvanize.com>
2024-03-15 08:35:45 -0700

    changed user schema on the backend

--- Back-End/models/User.js
+++ Back-End/models/User.js
@@ -3,24 +3,32 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   uuid: {
     type: String,
-    unique: true,
+    unique: true
   },
   username: {
     type: String,
-    unique: true,
+    unique: true
   },
   points: {
     type: Number,
-    default: 0,
+    default: 100
   },
   totalRounds: {
     type: Number,
-    default: 0,
+    default: 0
   },
   totalCorrect: {
     type: Number,
-    default: 0,
+    default: 0
   },
+  Wins: {
+    type: Number,
+    default:0
+  },
+  Losses: {
+    type:Number,
+    default:0
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
