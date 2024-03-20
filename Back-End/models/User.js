146c86b542595b7def15cc88a3b81f97bbba154c
Christian Wu <christian.wu@galvanize.com>
2024-03-20 16:05:53 -0700

    report button alert done

--- Back-End/models/User.js
+++ Back-End/models/User.js
@@ -29,14 +29,6 @@ const userSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
-  wins: {
-    type: Number,
-    default: 0,
-  },
-  losses: {
-    type: Number,
-    default: 0,
-  }
 });
 
 module.exports = mongoose.model("User", userSchema);
