ab19d08ddbf94d4df048589f67937fb0ac3a5b04
Christian Wu <christianwu56@gmail.com>
2024-04-05 15:03:08 -0700

    changingthe root

--- Front-End/src/api.js
+++ Front-End/src/api.js
@@ -1,8 +1,8 @@
 // Server Testt
-const root = process.env.REACT_APP_SOCKET_URL
+// const root = process.env.REACT_APP_SOCKET_URL
 
 // Development
-  // const root = 'http://localhost:3001';
+  const root = 'http://localhost:3001';
 
 const API = {
   BugSubmit: `${root}/form/bug`,
